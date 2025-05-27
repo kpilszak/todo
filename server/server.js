d163b89fdbf63e53db15e8eed231e6e0cabb9b18
kpilszak <klaudia.pilszak@gmail.com>
2025-05-27 06:51:44 -0400

    Add db connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
 const app = express()
+const pool = require('./db')
+
+app.get('/todos', (req, res) => {
+    try {
+        // await
+    } catch (err) {
+        console.error(err)
+    }
+})
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
