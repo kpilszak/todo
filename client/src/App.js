e4c13bfec0fb7fa1200a278e757204e638b3aafc
kpilszak <klaudia.pilszak@gmail.com>
2025-05-26 20:08:59 -0400

    Create users and todos sql tables

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import ListHeader from './components/ListHeader'
 const App = () => {
   return (
     <div className="app">
-      <ListHeader listName={'🏝️ Holiday tick list'}/>
+      <ListHeader listName={'🏝️ Holiday tick list'} />
     </div>
   )
 }
