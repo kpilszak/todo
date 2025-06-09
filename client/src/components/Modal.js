988ee9a5738278181ce0d17042bc399a10508657
kpilszak <klaudia.pilszak@gmail.com>
2025-06-09 10:24:02 -0400

    Create Modal form

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,38 @@
 const Modal = () => {
+    const mode = 'create'
+
+    const handleChange = () => {
+        console.log('Changing')
+    }
+
     return (
-        <div>
+        <div className="overlay">
+            <div className="modal">
+                <div className="form-title-container">
+                    <h3>Let's {mode} you task</h3>
+                    <button>X</button>
+                </div>
+                <form>
+                    <input 
+                        required
+                        maxLength={30}
+                        placeholder="Your task goes here"
+                        name="title"
+                        value={""}
+                        onChange={handleChange}
+                    />
+                    <br/>
+                    <input 
+                        type="range"
+                        min="0"
+                        max="100"
+                        name="progress"
+                        value={""}
+                        onChange={handleChange}
+                    />
+                    <input className={mode} type="submit"/>
+                </form>
+            </div>
         </div>
     )
 }
