28dccfa50979b2341aa25b32374b8c4ebeea157b
kpilszak <klaudia.pilszak@gmail.com>
2025-06-16 11:56:22 -0400

    Add editData

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,8 @@ const App = () => {
 
   return (
     <div className="app">
-      <ListHeader listName={'🏝️ Holiday tick list'} />
-      {sortedTasks?.map((task) => <ListItem key={task.id} task ={task} />)}
+      <ListHeader listName={'🏝️ Holiday tick list'} getData={getData} />
+      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
     </div>
   )
 }
