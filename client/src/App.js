67a3d63630151b801d30100df933f26c0d222fd0
kpilszak <klaudia.pilszak@gmail.com>
2025-06-24 21:14:14 -0400

    Add auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import ListHeader from './components/ListHeader'
 import ListItem from './components/ListItem'
+import Auth from './components/Auth'
 import { useEffect, useState } from 'react'
 
 const App = () => {
   const userEmail = 'ania@test.com'
   const [ tasks, setTasks ] = useState(null)
+
+  const authToken = false
 
   const getData = async () => {
     try {
@@ -17,15 +20,21 @@ const App = () => {
     }
   }
 
-  useEffect(() => getData, [])
+  useEffect(() => {
+    if (authToken) {
+      getData()
+    }}, [])
 
   console.log(tasks)
   const sortedTasks = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date))
 
   return (
     <div className="app">
+      {!authToken && <Auth/>}
+      {authToken  && 
+      <>
       <ListHeader listName={'🏝️ Holiday tick list'} getData={getData} />
-      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
+        {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}</>}
     </div>
   )
 }
