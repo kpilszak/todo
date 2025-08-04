df0403d1826ce83da332b59d59e0f9392aafe18d
kpilszak <klaudia.pilszak@gmail.com>
2025-08-04 10:33:35 -0400

    Greeting user message

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,9 @@ const App = () => {
       {authToken  && 
       <>
       <ListHeader listName={'🏝️ Holiday tick list'} getData={getData} />
-        {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}</>}
+      <p className="user-email">Welcome back {userEmail}</p>
+      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}</>}
+      <p className="copyright">© Creative Coding LLC</p>
     </div>
   )
 }
